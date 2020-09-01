07ea24f942a828a069a5e57c055bd6b3a639f3ab
Matias Turunen <mt95@windowslive.com>
2020-09-01 17:42:33 +0300

    Add selectors for graph data

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -10,6 +10,10 @@ const app = new Vue({
       pres: [],
       logged: [],
     },
+    timeFrom: '',
+    timeTo: '',
+    dataEntries: 10,
+    message: '',
   },
   mounted: function () {
     const accessToken = Cookies.get('accessToken');
@@ -19,12 +23,19 @@ const app = new Vue({
         this.datesmin = data.mi;
         this.datesmax = data.ma;
 
+        // Update date selectors to have only values that are in DB
+        $('#weather-timeFrom').attr('min', this.datesmin.slice(0, -5)); // Trim timezone
+        $('#weather-timeFrom').attr('max', this.datesmax.slice(0, -5));
+        $('#weather-timeTo').attr('min', this.datesmin.slice(0, -5));
+        $('#weather-timeTo').attr('max', this.datesmax.slice(0, -5));
+
         apiGetWeatherHistory(accessToken, this.datesmin, this.datesmax, 20)
           .done(w => {
             this.weatherData = w;
             this.updateCharts();
             console.log('WW', w);
           })
+          .fail(err => console.error(err));
       })
       .fail(err => console.error(err));
   },
@@ -81,12 +92,30 @@ const app = new Vue({
       });
     },
     groupChartData: function () {
+      this.weatherDataGrouped.temp = [];
+      this.weatherDataGrouped.hum = [];
+      this.weatherDataGrouped.pres = [];
+      this.weatherDataGrouped.logged = [];
+
       for (var i = 0; i < this.weatherData.length; i++) {
         this.weatherDataGrouped.temp.push(this.weatherData[i].temp)
         this.weatherDataGrouped.hum.push(this.weatherData[i].humidity)
         this.weatherDataGrouped.pres.push(this.weatherData[i].pressure)
         this.weatherDataGrouped.logged.push(this.weatherData[i].logged)
       }
+    },
+    loadWeather: function () {
+      const accessToken = Cookies.get('accessToken');
+      if (!this.dataEntries || this.dataEntries < 5 || this.dataEntries > 1000) {
+        this.dataEntries = 10;
+      }
+      apiGetWeatherHistory(accessToken, this.timeFrom, this.timeTo, this.dataEntries)
+        .done(w => {
+          this.weatherData = w;
+          this.updateCharts();
+          console.log('WW', w);
+        })
+        .fail(err => console.error(err));
     }
   }
 });
